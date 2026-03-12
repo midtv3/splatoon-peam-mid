08a26c791527f0617eea3f22e72bc85177cdcb81
MidTVU <midtvu@proton.me>
2026-03-12 10:39:55 -0500

    Update get_user_friend_pids.go

--- globals/get_user_friend_pids.go
+++ globals/get_user_friend_pids.go
@@ -3,7 +3,7 @@ package globals
 import (
 	"context"
 
-	pbfriends "github.com/PretendoNetwork/grpc/go/friends"
+	pbfriends "github.com/PeamoNetwork/grpc/go/friends"
 	"github.com/PretendoNetwork/nex-protocols-go/v2/globals"
 	"google.golang.org/grpc/metadata"
 )
